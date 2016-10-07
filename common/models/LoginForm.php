98172b80e399dd9398f23e6e1bc7e0d90f02a20d
Juan Felipe <juanchofelipe@gmail.com>
2016-10-06 21:21:39 -0500

    Backend login

--- common/models/LoginForm.php
+++ common/models/LoginForm.php
@@ -55,7 +55,7 @@ class LoginForm extends Model
      */
     public function login()
     {
-        if ($this->validate()) {
+        if ($this->validate() && $this->getUser()->status_id == ValueHelpers::getStatusValue('Active')) {
             return Yii::$app->user->login($this->getUser(), $this->rememberMe ? 3600 * 24 * 30 : 0);
         } else {
             return false;
@@ -74,5 +74,15 @@ class LoginForm extends Model
         }
 
         return $this->_user;
+    }
+
+    public function loginAdmin()
+    {
+        if (($this->validate()) && $this->getUser()->role_id >= ValueHelpers::getRoleValue('Admin')
+                && $this->getUser()->status_id == ValueHelpers::getStatusValue('Active')) {
+            return Yii::$app->user->login($this->getUser(), $this->rememberMe ? 3600 * 24 * 30 : 0);
+        } else {
+            throw new NotFoundHttpException('You Shall Not Pass.');
+        }
     }
 }
