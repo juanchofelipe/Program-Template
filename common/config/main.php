249d254be602f4ed8f239dd7970d5465f6f1a0b7
Juan Felipe <juanchofelipe@gmail.com>
2016-10-17 18:21:37 -0500

    Index Page

--- common/config/main.php
+++ common/config/main.php
@@ -1,6 +1,31 @@
 <?php
 return [
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+    'extensions' => require(__DIR__ . '/../../vendor/yiisoft/extensions.php'),
+    'modules' => [
+        'social' => [
+            'class' => 'kartik\social\Module',
+            'disqus' => [
+                'settings' => ['shortname' => 'DISQUS_SHORTNAME']
+            ],
+            'facebook' => [
+                'appId' => '205382639884285',
+                'secret' => 'e2e92ac7cc6621294f945e4002f2e02a'
+            ],
+            'google' => [
+                'clientId' => 'GOOGLE_API_CLIENT_ID',
+                'pageId' => 'GOOGLE_PLUS_PAGE_ID',
+                'profileId' => 'GOOGLE_PLUS_PROFILE_ID',
+            ],
+            'googleAnalytics' => [
+                'id' => 'TRACKING_ID',
+                'domain' => 'TRACKING_DOMAIN'
+            ],
+            'twitter' => [
+                'screenName' => 'TWITTER_SCREEN_NAME'
+            ]
+        ]
+    ],
     'components' => [
         'cache' => [
             'class' => 'yii\caching\FileCache',
